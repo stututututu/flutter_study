4f0bb26c19a21e56ad6fbba51547746661ab17e5
stututututu <stututu0602@gmail.com>
2023-06-10 15:06:14 +0900

    AstonMartin

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() => runApp(MyApp());
 
@@ -27,15 +27,102 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Study'),
+        title: Text(
+          '007',
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        foregroundColor: Colors.black,
         centerTitle: true,
-        backgroundColor: Colors.black,
+        backgroundColor: Color.fromARGB(255, 250, 249, 249),
         elevation: 0.0,
       ),
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(15, 30, 0.0, 0.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[Text('data'), Text('data')],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('images/2531980.jpg'),
+                radius: 60,
+              ),
+            ),
+            Divider(
+              height: 50,
+              color: Colors.black,
+              thickness: 0.5,
+              endIndent: 20,
+            ),
+            Text(
+              'James Bonds car',
+              style: TextStyle(
+                  color: Colors.black, letterSpacing: 4.0, fontSize: 20.0),
+            ),
+            SizedBox(
+              height: 0,
+            ),
+            Divider(
+              height: 60,
+              color: Colors.black,
+              thickness: 0.5,
+              endIndent: 20,
+            ),
+            Text(
+              'Aston Martin DB5',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                Text(
+                  'Wheelbase, 2,489 mm Length',
+                  style: TextStyle(fontSize: 16, letterSpacing: 1),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                Text(
+                  'Length, 4,570 mm Width, 1,680 mm',
+                  style: TextStyle(fontSize: 16, letterSpacing: 1),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                Text(
+                  ' DOHC Straight-6, 4.0 L (3,995 cc)',
+                  style: TextStyle(fontSize: 16, letterSpacing: 1),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                Text(
+                  '282–325 bhp',
+                  style: TextStyle(fontSize: 16, letterSpacing: 1),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
